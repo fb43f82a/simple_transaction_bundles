c175553ce3170081bd6f5d7efa62b560c0b1ba6b
lol <dcef451cabe506248d5ff888ee209449@protonmail.com>
2021-12-01 20:11:36 +0100

    initial version

--- src/Simpletransactionbundles.sol
+++ src/Simpletransactionbundles.sol
@@ -1,5 +1,54 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
+/// @title Contracts "check_state" function is supposed to be called as the last transaction in a flashbots bundle
+/// @dev This works by setting all transaction costs to a minimum. If the state is as expected, we pay the miner to include our bundle.
 contract Simpletransactionbundles {
+    address owner;
+
+    // empty constructor
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /// @notice This functions queries blockchain state. If the state is as expected, we pay the miner.
+    /// @dev SECURITY WARNING: do not call this for anything else but checking the state of the chain after your transactions.
+    function check_state(uint mineBy, uint minerPayment, bytes[] memory extcalldata) public payable {
+
+        // transaction expired, we do not want the bundle to be mined
+        require(block.number <= mineBy, "N");
+
+        uint len = extcalldata.length;
+        for (uint i = 0; i < len; i++) {
+            (address addr, bytes32 expected, bytes memory arguments) = abi.decode(extcalldata[i], (address, bytes32, bytes));
+
+            // call externally and get chain state
+            (, bytes memory data) = (addr).call(arguments);
+
+            // check the state against expected values
+            require(keccak256(data) == expected, "S");
+
+        }
+
+        // every check passed: pay the miner
+        block.coinbase.transfer(minerPayment);
+    }
+
+    /// @notice this is only needed to rescue users that misused "check_state"
+    function rescue(bytes[] memory extcalldata) public payable {
+        require(msg.sender == owner);
+
+        uint len = extcalldata.length;
+        for (uint i = 0; i < len; i++) {
+            (address addr, bytes32 expected, bytes memory arguments) = abi.decode(extcalldata[i], (address, bytes32, bytes));
+
+            // call externally
+            (, bytes memory data) = (addr).call(arguments);
+        }
+
+        uint bal = address(this).balance;
+        if (bal > 0) {
+            payable(owner).transfer(bal);
+        }
+    }
 }
--- src/Simpletransactionbundles.t.sol
+++ src/Simpletransactionbundles.t.sol
@@ -5,18 +5,168 @@ import "ds-test/test.sol";
 
 import "./Simpletransactionbundles.sol";
 
+contract CallReceiver {
+    uint stateA = 0;
+    uint stateB = 0;
+
+    function changeStateA(uint newval) public {
+        stateA = newval;
+    }
+    function changeStateB(uint newval) public {
+        stateB = newval;
+    }
+
+
+    function checkStateA() public returns (uint) {
+        return stateA;
+    }
+
+    function checkStateB() public returns (uint) {
+        return stateB;
+    }
+
+   function returns1() public returns (uint256) {
+        return 1;
+    }
+
+    function reverts(uint bla) public {
+        revert();
+    }
+
+}
+
 contract SimpletransactionbundlesTest is DSTest {
-    Simpletransactionbundles bundles;
+    Simpletransactionbundles executor;
+    CallReceiver receiver;
+
+    event EventNum(uint);
+    event Balance(uint);
 
     function setUp() public {
-        bundles = new Simpletransactionbundles();
+        executor = new Simpletransactionbundles();
+        receiver = new CallReceiver();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
+    function testFail_wrong_return() public {
 
-    function test_basic_sanity() public {
+        uint256 _expectedReturn = 0; // should be one to pass
+        bytes32 expectedReturn = keccak256(abi.encode(_expectedReturn));
+        bytes memory argument = abi.encodeWithSelector(CallReceiver.returns1.selector, "");
+
+
+        bytes memory _extCallData = constructCall(address(receiver),
+                        expectedReturn,
+                        argument);
+                    
+        bytes[] memory arr = new bytes[](1);
+        arr[0] = _extCallData;
+
+
+        executor.check_state{value: 1}(block.number, 1, arr);
+
         assertTrue(true);
     }
+
+    function testFail_call_reverts() public {
+       uint256 _dummyUint = 0; 
+       bytes32 expectedReturn = keccak256(abi.encode(_dummyUint)); // we don't actually care
+       bytes memory argument = abi.encodeWithSelector(CallReceiver.reverts.selector, _dummyUint);
+
+
+       bytes memory _extCallData = constructCall(address(receiver),
+                       expectedReturn,
+                       argument);
+                   
+       bytes[] memory arr = new bytes[](1);
+       arr[0] = _extCallData;
+
+       executor.check_state{value: 1}(block.number, 1, arr);
+
+       assertTrue(true);
+    }
+
+    function testFail_wrong_blocknum() public {
+       uint256 _dummyUint = 0; 
+       bytes32 expectedReturn = keccak256(abi.encode(_dummyUint)); // we don't actually care
+       bytes memory argument = abi.encodeWithSelector(CallReceiver.reverts.selector, _dummyUint);
+
+       bytes memory _extCallData = constructCall(address(receiver),
+                       expectedReturn,
+                       argument);
+                   
+       bytes[] memory arr = new bytes[](1);
+       arr[0] = _extCallData;
+
+       executor.check_state{value: 1}(block.number-1, 1, arr);
+
+       assertTrue(true);
+    }
+
+    function test_success_1_statechange() public {
+        CallReceiver receiver1StateChange = new CallReceiver();
+        uint balanceBefore = address(this).balance;
+
+        // this would be the result of a previous transactions inside of the bundle
+        receiver1StateChange.changeStateA(9999);
+
+        
+        uint256 newStateValA = 9999; 
+        bytes32 expectedReturn = keccak256(abi.encode(newStateValA)); // we don't actually care
+        bytes memory argument = abi.encodeWithSelector(CallReceiver.checkStateA.selector, newStateValA);
+
+
+        bytes memory _extCallData = constructCall(address(receiver1StateChange),
+                        expectedReturn,
+                        argument);
+
+        bytes[] memory arr = new bytes[](1);
+        arr[0] = _extCallData;
+
+        executor.check_state{value: 1}(block.number, 1, arr);
+
+        emit Balance(balanceBefore);
+        emit Balance(address(this).balance);
+        assertTrue(balanceBefore == address(this).balance + 1);
+    }
+
+    function test_success_2_statechanges() public {
+        CallReceiver receiver2StateChange = new CallReceiver();
+
+        uint balanceBefore = address(this).balance;
+
+        // this would be the result of a previous transactions inside of the bundle
+        receiver2StateChange.changeStateA(9999);
+        receiver2StateChange.changeStateB(8888);
+
+        
+        uint256 newStateValA = 9999; 
+        bytes32 expectedReturnA = keccak256(abi.encode(newStateValA)); // we don't actually care
+        bytes memory argumentA = abi.encodeWithSelector(CallReceiver.checkStateA.selector, newStateValA);
+        bytes memory _extCallDataA = constructCall(address(receiver2StateChange),
+                        expectedReturnA,
+                        argumentA);
+
+        uint256 newStateValB = 8888; 
+        bytes32 expectedReturnB = keccak256(abi.encode(newStateValB)); // we don't actually care
+        bytes memory argumentB = abi.encodeWithSelector(CallReceiver.checkStateB.selector, newStateValB);
+        bytes memory _extCallDataB = constructCall(address(receiver2StateChange),
+                        expectedReturnB,
+                        argumentB);
+
+
+        bytes[] memory arr = new bytes[](2);
+        arr[0] = _extCallDataA;
+        arr[1] = _extCallDataB;
+
+        executor.check_state{value: 1}(block.number, 1, arr);
+
+        emit Balance(balanceBefore);
+        emit Balance(address(this).balance);
+        assertTrue(balanceBefore == address(this).balance + 1);
+    }
+
+    function constructCall(address addr, bytes32 expectedReturn, bytes memory args) public returns (bytes memory) {
+        return abi.encode(addr, expectedReturn, args);
+    }
+
 }
